d1c1bc485cd98fc71403b7c188e7166b791cc2b7
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2020-10-13 09:53:23 +0100

    Fix problem when using GPUs

--- entrypoint_pytorch.sh
+++ entrypoint_pytorch.sh
@@ -6,4 +6,5 @@ if [ -z "$RUNAI_MPI_NUM_WORKERS" ]; then
 fi
 
 echo "Running with $RUNAI_MPI_NUM_WORKERS workers"
-horovodrun -np $RUNAI_MPI_NUM_WORKERS python /examples/pytorch_synthetic_benchmark.py
+# The script is available at https://github.com/horovod/horovod/blob/master/examples/pytorch/pytorch_synthetic_benchmark.py
+horovodrun -np $RUNAI_MPI_NUM_WORKERS -hostfile /etc/mpi/hostfile  python /examples/pytorch_synthetic_benchmark.py
